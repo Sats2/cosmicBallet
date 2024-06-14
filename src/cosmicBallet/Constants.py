cb82d02d9d942be7577ebd12a98ddd6659a0f79f
Sats2 <sathyamhegde@gmail.com>
2024-06-14 22:40:02 +0200

    Constants updated to PEP8 standard

--- src/cosmicBallet/Constants.py
+++ src/cosmicBallet/Constants.py
@@ -1,19 +1,19 @@
 # All universal constants are in SI Units unless explicitly stated
 
 G = 6.67430e-11             # Universal Gravitational Constant
-Ms = 1.9884e30              # Solar Mass (Mass of the Sun)
-Me = 5.972e24               # Earth Mass (Mass of the Earth)
+MS = 1.9884e30              # Solar Mass (Mass of the Sun)
+ME = 5.972e24               # Earth Mass (Mass of the Earth)
 AU = 1.49598e11             # Astronomical Unit (Distance between the Earth and the Sun)
-ly = 9.4607e15              # Light Year (Distance Travelled by Light in 1 year)
-c = 2.99792458e8            # Speed of Light
+LY = 9.4607e15              # Light Year (Distance Travelled by Light in 1 year)
+C = 2.99792458e8            # Speed of Light
 
 
 
 # Conversion Factors
 
-lbs_to_kg = 0.4535924       # Conversion from pounds (lbs) to kilograms (kg)
-ftps_to_mtps = 0.3048       # Conversion from feet/second (ft/s) to meter/second (m/s)
-mph_to_mtps = 0.44704       # Conversion from miles/second (mile/s) to meter/second (m/s)
-kmph_to_mtps = 0.2777778    # Conversion from kilometers/hour (km/hr) to meter/second (m/s)
-mile_to_mt = 1609.344       # Conversion from miles (mi) to meters (m)
-km_to_mt = 1000.0           # Conversion from kilometer (km) to meters (m) 
+LBS_TO_KG = 0.4535924       # Conversion from pounds (lbs) to kilograms (kg)
+FTPS_TO_MTPS = 0.3048       # Conversion from feet/second (ft/s) to meter/second (m/s)
+MPH_TP_MTPS = 0.44704       # Conversion from miles/second (mile/s) to meter/second (m/s)
+KMPH_TO_MTPS = 0.2777778    # Conversion from kilometers/hour (km/hr) to meter/second (m/s)
+MILE_TO_MT = 1609.344       # Conversion from miles (mi) to meters (m)
+KM_TO_MT = 1000.0           # Conversion from kilometer (km) to meters (m) 
