18979c97ae96030ed744139541e93d4a94bbd80d
Sats2 <sathyamhegde@gmail.com>
2024-07-10 14:38:23 +0200

    Code Updates to methods

--- src/cosmicBallet/Visualization.py
+++ src/cosmicBallet/Visualization.py
@@ -1,5 +1,6 @@
 import Constants as const
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 from typing import Union
 import numpy as np
 
@@ -13,7 +14,7 @@ class Visualize():
         save_fig (bool): A user input to determine whether to save the trajectory visualization. Defaults to False
         figure_name (str): The name for the figure to be saved. Defaults to None
     """
-    def __init__(self, simulation_result:object, visualization_type:str="scientific", save_figure:bool=False,
+    def __init__(self, celestial_objects:list, visualization_type:str="scientific", save_figure:bool=False,
                  figure_name:str=None) -> None:
         """Constructor for the Visualization class
 
@@ -28,7 +29,7 @@ class Visualize():
             ValueError: Raised when visualization_type does not belong to the allowed value.
         """
         try:
-            assert isinstance(simulation_result, object), "The simulation result needs to be an object of one of the Simulation classes"
+            assert isinstance(celestial_objects, object), "The celestial_objects needs to be a list of objects for visualization"
             assert isinstance(visualization_type, str), "The visualization_type needs to be a string"
             assert isinstance(save_figure, bool), "The attribute save_figure must be a boolean"
             assert isinstance(figure_name, str), "The figure_name must be a string"
@@ -39,33 +40,41 @@ class Visualize():
             assert (visualization_type.lower() in accepted_choices), "The visualization type must be either scientific/animation"
         except AssertionError:
             raise ValueError
-        self.results = simulation_result
+        self.results = celestial_objects
         self.visual_type = visualization_type
         self.save_fig = save_figure
         self.figure_name = figure_name
+
+    def __create_animation(self):
+        pass
     
-    def __scientific_plot(self)->None:
+    def __scientific_plot(self, animate:bool)->None:
         """Private method of the Visualization class that generates a maplotlib plot of the trajectory of the bodies.
 
         Raises:
             ValueError: When no trajectory for the celestial objects are found
         """
         try:
-            assert (self.results.positions is not None), "No trajectory found. Please run the simulation before visualization"
+            for body in self.celestial_objects:
+                assert body.trajectory!=[], f"Empty trajectory found for {body.name}. Run Simulations first"
         except AssertionError:
             raise ValueError
-        solution = np.array(self.results.positions)
-        fig = plt.figure(figsize=(20,14))
+        if animate:
+            self.__create_animation()
+        fig = plt.figure()
         ax = fig.add_subplot(111, projection="3d")
-        for i in range(len(self.results.celestial_bodies)):
-            body_result = solution[i]
-            body_name = self.results.celestial_bodies.name
-            ax.plot(body_result[:,0], body_result[:,1], body_result[:,2], label=body_name)
+        i = 1
+        for body in self.celestial_objects:
+            trajectory = np.array(body.trajectory)
+            if body.object_type != "fragment":
+                ax.plot(trajectory[:,0], trajectory[:,1], trajectory[:,2], label=body.name)
+            else:
+                ax.plot(trajectory[:,0], trajectory[:,1], trajectory[:,2], label=f"fragment{i}")
+                i += 1
         plt.legend()
-        plt.xlabel("x [m]")
-        plt.ylabel("y [m]")
-        plt.zlabel("z [m]")
-        plt.title(f"Orbital Trajectory for {len(self.results.celestial_bodies)} Bodies for Time [0, {self.results.simulation_time}]")
+        ax.set_xlabel("x [m]")
+        ax.set_ylabel("y [m]")
+        ax.set_zlabel("z [m]")
         if self.save_fig:
             plt.savefig(self.figure_name, dpi=300)
         plt.show()
@@ -73,20 +82,23 @@ class Visualize():
     def __animation(self)->None:
         pass
     
-    def visualize(self, time_interval:Union[float,int]=None)->None:
+    def visualize(self, animate:bool=False, time_interval:Union[float,int]=None)->None:
         """Method of the visualization class that generates the visualization for the trajectories based on the visualization type
 
         Args:
-            time_interval (float/int, optional): Required on for animations to determine the time interval between each frame. Defaults to None.
+            animate (bool): Used for scientific visualization and control whether an animation is generated.
+            time_interval (float/int, optional): Required on for animations to determine the time interval between each frame. 
+                                                Defaults to None.
 
         Raises:
             ValueError: When the time_interval is not specified for the animation render.
         """
         try:
+            assert isinstance(animate, bool), "animate can only be set to True/False"
             assert (isinstance(time_interval, (float,int)) and self.visual_type == "animation"), "Time Interval cannot be None for Animation Renders"
         except AssertionError:
             raise ValueError
         if self.visual_type == "scientific":
-            self.__scientific_plot()
+            self.__scientific_plot(animate=animate)
         else:
             self.__animation()
