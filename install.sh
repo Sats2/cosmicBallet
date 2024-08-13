34cd4e73265a6d21b982190ec7a291ca52a922f9
Sathyamurthy Hegde <118522128+Sats2@users.noreply.github.com>
2024-08-13 15:32:25 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 conda create --name cosmicBallet --file spec-file-optional.txt
 conda init
-conda activate cosmicball
+conda activate cosmicBallet
 pip install NRSur7dq2
 python3 -m pip install .
 echo "Completed Environment Setup"
